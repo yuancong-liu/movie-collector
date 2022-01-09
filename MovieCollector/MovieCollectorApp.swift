36408f2a27bccd88c309c10ad95aa0c65670723b
YUAN-TSUNG <satoshi.liu@icloud.com>
2022-01-10 00:55:29 +0800

    Initial Commit

---
+++ MovieCollector/MovieCollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCollectorApp.swift
+//  MovieCollector
+//
+//  Created by YUAN-TSUNG LIOU on 2022/1/10.
+//
+
+import SwiftUI
+
+@main
+struct MovieCollectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
